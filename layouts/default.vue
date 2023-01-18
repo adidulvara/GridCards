d2303dccf7af993d5b51e2a475810bc815733023
Adi Dulvara <37400015+adidulvara@users.noreply.github.com>
2023-01-18 17:33:56 +0200

    Footer v2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
    <div>
-    <div class="font-dm flex flex-col mx-10 md:mx-24 lg:mx-auto max-w-screen-lg h-screen">
+    <div class="font-dm flex flex-col mx-10 md:mx-24 lg:mx-auto max-w-screen-lg ">
     <Navbar v-if="!onLoginPage"/>
         <slot/>
     </div>
+    <Footer v-if="!onLoginPage"/>     
    </div>
 </template>
 <script setup lang="ts">
